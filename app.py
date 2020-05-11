883e39a01e54e746594078941899f7222c7875b9
AR_JPEG <58893337+AR-jpeg@users.noreply.github.com>
2020-05-11 17:04:30 -0500

    Added more routes

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
 """Main File."""
-
-print("\n"*2)
-print("Look at the website @")
-
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from datetime import datetime
+import random
+
+
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=hex(__import__('random').randint(0,2_147_483_647)))  # 2_147_483_647 is the largest hex number
+class User(db.Model): # work in progress...
+    """Base class for users."""
+
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    hashed_password = db.Column(db.String(400))
+    primary_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(1000), unique=True)
+
+    def __repr__(self):
+        """Represent the given user."""
+        return f'<User {self.username}>'
+
 
 class BlogPost(db.Model):
     """Base data base model."""
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(30), nullable=False, default="N/A")
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    author = db.Column(db.String(30), nullable=False, default="N/A")
+    title = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
 
     date_posted_fancy = "PLACEHOLDER" 
 
@@ -34,26 +46,59 @@ def index():
     """Index route (ex) yourdomain.extension/."""
     return render_template('index.html')
 
-@app.route("/users/<string:name>")
-def main(name, id):
-    """User/name's info."""
-    return f'{name}, posted a post with id `{id}``'
+@app.errorhandler(404)
+def error(e):
+    """Handle errors."""
+    return render_template('404.html'), 404
+
+@app.route('/signup/new', methods=['POST'])
+def new_signup():
+    """Signup."""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        new_user = User(username=username, hashed_password=hashed_password, email=email, id=hex(random.randint(100_100, 999_999)))
+        print(type(new_user.hashed_password))
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(f'/user/{new_user.id}')
+    else:
+        return redirect('/signup')
+
+@app.route('/signup')
+def signup():
+    """Render template."""
+    return render_template('signup.html')
 
 @app.route('/posts', methods=['GET', 'POST'])
 def posts():
     """Render all posts."""
-    if request.method == "POST":
-        title = request.form['title']
-        content = request.form['content']
-        author = request.form['author']
+    all_posts = BlogPost.query.all()
+    return render_template('posts.html', posts=all_posts)
 
-        new_post = BlogPost(title=title, content=content, author=author)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
+@app.route('/user/<id>')
+def user(id_number, is_logged_in = False):
+    """Show a user."""
+    try:
+        user = User.query.all()
+    except:
+        pass
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    """Login to a given user."""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
     else:
-        all_posts = BlogPost.query.all()
-        return render_template('posts.html', posts=all_posts)
+        return render_template('login.html')
 
 @app.route('/posts/delete/<int:id>')
 def delete(id):
@@ -99,4 +144,5 @@ def new_post():
         return render_template('new_post.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    db.create_all()
+    app.run(debug=True)
