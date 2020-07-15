be49dbf2e41c6fb27f290eac8fc0b13791317551
AR_JPEG <58893337+AR-jpeg@users.noreply.github.com>
2020-07-15 13:19:38 -0500

    Added try/except block

--- run.py
+++ run.py
@@ -3,7 +3,10 @@ from waitress import serve
 from src import app
 import socket
 
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
 
-serve(app, host=ip_address, port=80)
+try:
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    serve(app, host=ip_address, port=80)
+except:
+    serve(app)
